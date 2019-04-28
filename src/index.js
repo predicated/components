f0739306297fe3f99dcb7951e279a378c49fe9a1
Michael N. Rubinstein <michael@polyblog.com>
2019-04-27 17:37:16 -0700

    Completed Section 3

--- src/index.js
+++ src/index.js
@@ -2,13 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return  (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:50pm" content="That's, like, groovy man!" avatarImg={faker.image.avatar()} />
-            <CommentDetail author="Alex" timeAgo="Today at 2:00am" content="This is totally not fake." avatarImg={faker.image.avatar()} />
-            <CommentDetail author="Jane" timeAgo="Yestrerday at 5:000pm" content="Gort! Klaatu burata cheese!" avatarImg={faker.image.avatar()} />
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:50pm" 
+                    content="That's, like, groovy man!" 
+                    avatarImg={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 2:00am" 
+                    content="This is totally not fake." 
+                    avatarImg={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Yestrerday at 5:000pm" 
+                    content="Gort! Klaatu burata nikto!" 
+                    avatarImg={faker.image.avatar()} 
+                />
+            </ApprovalCard>
         </div>
     );
 };
