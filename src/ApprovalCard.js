82679afd594c194e273bf16b63f47467a0918d84
Michael N. Rubinstein <michael@polyblog.com>
2019-08-21 19:03:36 -0700

    Placed Approve/Reject buttons inside a Two Buttons css class

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -6,8 +6,10 @@ const ApprovalCard = (props) => {
         <div className="ui card">
             <div className="content">{props.children}</div>
             <div className="extra content">
-                <div className="ui basic green button">Approve</div>
-                <div className="ui basic red button">Reject</div>
+                <div className="ui two buttons">
+                    <div className="ui basic green button">Approve</div>
+                    <div className="ui basic red button">Reject</div>
+                </div>
             </div>
         </div>
     );
